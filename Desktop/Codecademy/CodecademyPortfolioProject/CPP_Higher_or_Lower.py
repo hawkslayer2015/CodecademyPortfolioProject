1661ea6cdb5f8621c025743d5e13ef104d225d53
hawkslayer2015 <hawkslayer2015@gmail.com>
2024-11-15 16:35:25 +0000

    Commented out game title and description added

---
+++ Desktop/Codecademy/CodecademyPortfolioProject/CPP_Higher_or_Lower.py
@@ -0,0 +1,5 @@
+# 'Higher or Lower' - My First Codecademy Portfolio Project.
+# A simple 'higher or lower' terminal based card game.
+# A player is dealt a card and has to guess whether the following card will have a higher or lower value (ie. 10 of clubs is higher than 7 of diamonds).
+# As the game progresses, the player's skill level will increase or decrease depending on the success of their guesses.
+# ----------
