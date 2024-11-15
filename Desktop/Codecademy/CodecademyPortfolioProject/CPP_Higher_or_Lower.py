63b251a70bc6a824e44a5e4ac9ae256632dc19ee
hawkslayer2015 <hawkslayer2015@gmail.com>
2024-11-15 17:00:08 +0000

    Created 'Player' class

--- Desktop/Codecademy/CodecademyPortfolioProject/CPP_Higher_or_Lower.py
+++ Desktop/Codecademy/CodecademyPortfolioProject/CPP_Higher_or_Lower.py
@@ -3,3 +3,14 @@
 # A player is dealt a card and has to guess whether the following card will have a higher or lower value (ie. 10 of clubs is higher than 7 of diamonds).
 # As the game progresses, the player's skill level will increase or decrease depending on the success of their guesses.
 # ----------
+class Player:
+    def __init__(self, name="Player 1"):
+        self.name = name
+        self.skill_level = 1
+        self.has_played = 0
+        self.has_won = 0
+        self.has_lost = 0
+
+    def __repr__(self):
+        pass
+
